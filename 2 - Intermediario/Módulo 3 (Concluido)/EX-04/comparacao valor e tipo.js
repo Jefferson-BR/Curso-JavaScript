81b7f66a404f6c9932317af6daea390dea2f1bef
Jefferson Barbosa <119863688+Jefferson-BR@users.noreply.github.com>
2024-05-08 10:33:59 -0300

    Add files via upload

--- 2 - Intermediario/Módulo 3 (Concluido)/EX-04/comparacao valor e tipo.js
+++ 2 - Intermediario/Módulo 3 (Concluido)/EX-04/comparacao valor e tipo.js
@@ -2,13 +2,19 @@
 var numero = '5';
 
 // Verifica se o valor de numero é igual a 5 e do mesmo tipo
-if (numero === 5) {
+if (numero === 5 || numero === '5') {
     // Se verdadeiro, imprime 'O Numero 5 é do mesmo tipo'
-    console.log('O Numero 5 é do mesmo tipo');
+    console.log('O Número 5 é do mesmo tipo, e tem o mesmo valor');
 }
 
 // Verifica se o valor de numero é estritamente diferente de '5'
-if (numero !== '5') {
+else if (numero !== '5') {
     // Se verdadeiro, imprime 'O Numero 5 não é do mesmo tipo'
-    console.log('O Numero 5 não é do mesmo tipo');
+    console.log('O Número tem o mesmo tipo, mas não é do mesmo valor');
+}
+
+// Verifica se o valor de numero é diferente de 5 ou se o tipo é diferente de string
+if (numero !== 5 || typeof numero !== 'string') {
+    // Se verdadeiro, imprime 'O Numero é de tipo diferente, mas não tem o mesmo valor'
+    console.log('O Número é de tipo diferente, mas não tem o mesmo valor');
 }
